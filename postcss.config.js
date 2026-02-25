7697ab7826004ad1feb152b97fa10ddfdb6921b6
NKwelle-destiny <mkwelledestiny923306@gmail.com>
2026-02-25 20:09:54 +0100

    first commit

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+const autoprefixer = require("autoprefixer");
+const tailwindConfig = require("./tailwind.config");
+
+module.exports={
+    plugins:{
+        tailwindcss:{},
+        autoprefixer:{},
+    },
+}
